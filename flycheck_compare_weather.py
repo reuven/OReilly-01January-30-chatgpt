e205f883c0427099129bfecd17a83533e4b571ca
Reuven Lerner <reuven@lerner.co.il>
2024-01-30 21:25:16 +0200

    gitautosync update 34

---
+++ flycheck_compare_weather.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import requests
+
+base_url = 'http://api.openweathermap.org/data/2.5/weather'
+
+params = {'q':'Chicago',
+          'appid':api_key,
+          'units':metric}
+
+r = requests.get(
